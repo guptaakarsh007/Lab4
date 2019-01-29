8ab66b8e32bd33555e86e21dc5a2ec98b38b9263
guptaakarsh007 <46920614+guptaakarsh007@users.noreply.github.com>
2019-01-29 18:01:35 +0530

    Add files via upload

---
+++ Businesslogic.java
@@ -0,0 +1,17 @@
+package com.capgemini.bl;
+import com.capgemini.beans.*;
+
+public class Businesslogic {
+	public String createaccount(String sname,int randno)
+	{
+		return "yes";
+	}
+
+	public String createaccount(String sname, int randno, int age) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
